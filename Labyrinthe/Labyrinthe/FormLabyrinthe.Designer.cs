21b052bc1c59012203b29ee5c3f4454ce3495ad6
alexis20 <lalonde.alexis@hotmail.com>
2015-03-16 19:16:35 -0400

    Main form and user control created

---
+++ Labyrinthe/Labyrinthe/FormLabyrinthe.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace Labyrinthe
+{
+    partial class FormLabyrinthe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormLabyrinthe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 662);
+            this.MinimumSize = new System.Drawing.Size(1150, 700);
+            this.Name = "FormLabyrinthe";
+            this.Text = "Le Labyrinthe";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
